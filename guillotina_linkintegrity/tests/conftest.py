d2c0ac95c2fdeb0c698315d994f802d89f405964
vangheem <vangheem@gmail.com>
2018-09-24 14:54:47 -0400

    cr version

--- guillotina_linkintegrity/tests/conftest.py
+++ guillotina_linkintegrity/tests/conftest.py
@@ -1,3 +1,11 @@
+from pytest_docker_fixtures import images
+
+
+images.configure(
+    'cockroach',
+    'cockroachdb/cockroach', 'v2.0.5')
+
+
 pytest_plugins = [
     'aiohttp.pytest_plugin',
     'guillotina.tests.fixtures',
